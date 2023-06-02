71946f6f7948bfd30b55e5396d2e6fd07e30ee35
Michelle Li <rphmichelle@gmail.com>
2023-06-02 15:50:37 -0400

    working on 03_queries

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/03_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/03_queries.rb
@@ -1,7 +1,7 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-  
+  Movie.select(:title, :id).joins(:actors).where.not('actors.name not in (?)', those_actors)
 end
 
 def golden_age
